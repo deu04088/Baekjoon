5da4a5231764ae2c6ec429efeb43e41552412d97
deu04088 <37111328+deu04088@users.noreply.github.com>
2025-09-11 17:10:05 +0900

    [Bronze III] Title: 대지, Time: 772 ms, Memory: 100640 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int min_x = 10000, min_y = 10000, max_x = -10000, max_y = -10000;
+
+        for(int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            if(x < min_x) {
+                min_x = x;
+            }
+            if(x > max_x) {
+                max_x = x;
+            }
+            if(y < min_y) {
+                min_y = y;
+            }
+            if(y > max_y) {
+                max_y = y;
+            }
+        }
+
+        if (n == 1) {
+            System.out.println(0);
+        }
+        else {
+            int result = (max_x - min_x) * (max_y-min_y);
+            System.out.println(result);
+        }
+    }
+}
